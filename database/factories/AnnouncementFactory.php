21bea878c72f9fbbc0f9ab32bc86e615183b32bf
ebrahimmostafa133 <ibrahim.mostafa9939@gmail.com>
2026-06-08 15:52:26 +0300

    fix(ANN-1): give factory announcements a track-admin author, revert UserFactory default

--- database/factories/AnnouncementFactory.php
+++ database/factories/AnnouncementFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Enums\UserRole;
 use App\Models\Announcement;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
@@ -16,7 +17,7 @@ class AnnouncementFactory extends Factory
     public function definition(): array
     {
         return [
-            'author_id' => User::factory(),
+            'author_id' => User::factory()->state(['role' => UserRole::TrackAdmin->value]),
             'title' => fake()->sentence(),
             'body' => fake()->paragraph(),
         ];
